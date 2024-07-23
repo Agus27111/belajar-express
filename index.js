969d341f1d78de57617b967045b263b6a0c7b45e
Agus Setiawan <Agussetiawan.belajar@gmail.com>
2024-07-23 23:47:02 +0700

    finish the BE

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
+//import express
 const express = require("express");
+
+//import CORS
 const cors = require("cors");
+
+//import bodyParser
 const bodyParser = require("body-parser");
 
+//import router
+const router = require("./routes");
+
+//init app
 const app = express();
+
+//use cors
 app.use(cors());
-// parse application/x-www-form-urlencoded
+
+//use body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-const route = 3000;
+//define port
+const port = 3000;
 
+//route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(route, () => console.log(`Running on port ${route}`));
+//define routes
+app.use("/api", router);
+
+//start server
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
